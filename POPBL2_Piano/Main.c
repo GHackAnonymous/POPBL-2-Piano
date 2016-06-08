3cbc68025f1bac799f40affd62342a5684608187
GHackAnonymous <ederaraia@gmail.com>
2016-06-08 09:54:30 +0200

    Inicializo los pines de la GPIO
    Inicializacion de los pines de la GPIO que vamos a utilizar
    para el teclado creado por nosotros.
    
    OJO!!! Si la placa electronica esta construida de esta forma
     1.-> tierra resistencia pinGPIO boton voltaje -> es al inverso, Es decir cuando al led se le pase 0 se encendera y con 1 se apagara.
    2.-> tierra boton pinGPIO resistencia voltaje ->En este caso no estaria a la inversa, es cecir cuando le pasa 1 se enciende el led y cuando le pasa 0 se apaga.
    
    Resurso: http://www.resistorguide.com/pull-up-resistor_pull-down-resistor/

--- POPBL2_Piano/Main.c
+++ POPBL2_Piano/Main.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <unistd.h>   //_getch*/
 #include <termios.h>  //_getch*/
+#include <wiringPi.h>
 
 int main()
 {       
@@ -21,6 +22,24 @@ int main()
 	//printf("->");
 	//fgets(str, 128, stdin);
 	//sscanf(str, "%d", &opcion);
+
+	if (wiringPiSetup() == -1)
+	{
+		return 1;
+	}
+
+	pinMode(0, INPUT); //0 es el correspondiente del chip al pin 11
+	pinMode(1, INPUT); //1 es el correspondiente del chip al pin 12
+	pinMode(2, INPUT); //2 es el correspondiente del chip al pin 13
+	pinMode(3, INPUT); //3 es el correspondiente del chip al pin 15
+	pinMode(4, INPUT); //4 es el correspondiente del chip al pin 16
+	pinMode(5, INPUT); //5 es el correspondiente del chip al pin 18
+	pinMode(6, INPUT); //6 es el correspondiente del chip al pin 22
+	pinMode(7, INPUT); //7 es el correspondiente del chip al pin 29
+	pinMode(8, INPUT); //8 es el correspondiente del chip al pin 31
+	pinMode(9, INPUT); //9 es el correspondiente del chip al pin 32
+	pinMode(10, INPUT); //10 es el correspondiente del chip al pin 33
+	pinMode(11, INPUT); //11 es el correspondiente del chip al pin 35
 
 
 
